84f709998963ea4bb40d1857971d52a512479a90
GowriKrishnamurthy <35420783+GowriKrishnamurthy@users.noreply.github.com>
2018-07-14 20:44:24 +1000

    Model with annotations added for Checking Account

---
+++ Models/CheckingAccount.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace AutomatedTellerMachine.Models
+{
+    public class CheckingAccount
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(10)]
+        [Column(TypeName = "varchar")]
+        [Display(Name = "Account #")]
+        public string AccountNumber { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        public string Name => $"{FirstName} {LastName}";
+
+        [DataType(DataType.Currency)]
+        public decimal Balance { get; set; }
+    }
+}
